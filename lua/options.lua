c1d9d4c6d12ef0d28cade45e4f59cc42619e2b72
jrroman <jrrmod@gmail.com>
2023-02-08 21:05:44 -0500

    add function to highlight column when chars surpass 80 columns

--- lua/options.lua
+++ lua/options.lua
@@ -5,4 +5,15 @@ o.expandtab = true
 o.shiftwidth = 4
 o.tabstop = 4
 o.number = true
-o.colorcolumn = "80"
+-- o.colorcolumn = "80"
+
+-- Highlight column only when characters surpass 80 columns
+vim.api.nvim_create_autocmd(
+    { "VimEnter", "WinEnter"},
+    { 
+        pattern = "*",
+        callback = function()
+            vim.fn.matchadd('ColorColumn', '\\%79v', 100)
+        end
+    }
+)
