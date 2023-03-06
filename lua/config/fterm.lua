1689eec51d5ae25b75c8e2b047c7b4698a6f6d4f
John Roman <john@sprig.com>
2023-03-05 22:45:38 -0500

    fterm window size

--- lua/config/fterm.lua
+++ lua/config/fterm.lua
@@ -3,7 +3,7 @@ local fterm = require("FTerm")
 fterm.setup({
     border = 'single',
     dimensions  = {
-        height = 0.7,
+        height = 0.9,
         width = 0.9,
     },
     clear_env = false,
