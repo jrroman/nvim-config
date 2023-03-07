917a6022f960f8454622d8371b5f141a550e049b
jrroman <john@sprig.com>
2023-03-07 00:17:28 -0500

    sync

--- lua/config/gruvbox-material.lua
+++ lua/config/gruvbox-material.lua
@@ -1,2 +1,3 @@
---vim.g.gruvbox_material_background = "hard"
+-- Options are "soft", "medium", "hard"; default is "medium"
+--vim.g.gruvbox_material_background = "soft"
 vim.cmd("colorscheme gruvbox-material")
