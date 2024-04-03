a4b4a52d5db201f2325fa27301d0a426e7823c40
John Roman <jrrmod@gmail.com>
2024-04-03 12:55:37 -0400

    ignore .git in finder

--- lua/config/nvim-tree.lua
+++ lua/config/nvim-tree.lua
@@ -10,6 +10,7 @@
 --end
 
 require("nvim-tree").setup({
+  hijack_cursor = false,
   sort = {
     sorter = "case_sensitive",
   },
@@ -30,5 +31,8 @@ require("nvim-tree").setup({
   },
   filters = {
     dotfiles = false,
+    custom = {
+      "^.git$",
+    },
   },
 })
