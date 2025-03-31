f7ff4ec6908f0738250457b670c3d02bc3f2c416
John Roman <jrrmod@gmail.com>
2025-03-31 18:28:31 -0400

    spacing and add color to the number row

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -1,68 +1,85 @@
 -- Highlight column only when characters surpass 80 columns
 vim.api.nvim_create_autocmd(
-    { "VimEnter", "WinEnter" },
-    { 
-        pattern = "*",
-        callback = function()
-            vim.cmd("highlight EightyChars ctermbg=lightred guibg=lightred ctermfg=black guifg=black")
-            vim.fn.matchadd('EightyChars', '\\%79v', 100)
-        end
-    }
+  { "VimEnter", "WinEnter" },
+  { 
+    pattern = "*",
+    callback = function()
+      vim.cmd("highlight EightyChars ctermbg=lightred guibg=lightred ctermfg=black guifg=black")
+      vim.fn.matchadd('EightyChars', '\\%79v', 100)
+    end
+  }
 )
+
+-- Here we set the color of the cursor number row, cursor color, and cursor line
+vim.api.nvim_create_autocmd(
+  { "VimEnter", "WinEnter" },
+  { 
+    pattern = "*",
+    callback = function()
+      vim.cmd("highlight CursorLineNr ctermbg=lightred guibg=lightred ctermfg=black guifg=black")
+    end
+  }
+)
+
 -- autocmd FileType javascript setlocal ts=2 sts=2 sw=2 expandtab 
 vim.api.nvim_create_autocmd(
-    { "FileType" },
-    {
-        pattern = "javascript",
-        command = "setlocal shiftwidth=4 softtabstop=4 expandtab",
-    }
+  { "FileType" },
+  {
+    pattern = "javascript",
+    command = "setlocal shiftwidth=4 softtabstop=4 expandtab",
+  }
 )
+
 -- autocmd FileType javascriptreact setlocal ts=2 sts=2 sw=2 expandtab 
 vim.api.nvim_create_autocmd(
-    { "FileType" },
-    {
-        pattern = "javascriptreact",
-        command = "setlocal shiftwidth=4 softtabstop=4 expandtab",
-    }
+  { "FileType" },
+  {
+    pattern = "javascriptreact",
+    command = "setlocal shiftwidth=4 softtabstop=4 expandtab",
+  }
 )
+
 -- autocmd FileType typescript setlocal ts=2 sts=2 sw=2 expandtab 
 vim.api.nvim_create_autocmd(
-    { "FileType" },
-    {
-        pattern = "typescript",
-        command = "setlocal sw=4 softtabstop=4 expandtab",
-    }
+  { "FileType" },
+  {
+    pattern = "typescript",
+    command = "setlocal sw=4 softtabstop=4 expandtab",
+  }
 )
+
 -- autocmd FileType typescriptreact setlocal ts=2 sts=2 sw=2 expandtab 
 vim.api.nvim_create_autocmd(
-    { "FileType" },
-    {
-        pattern = "typescriptreact",
-        command = "setlocal sw=4 softtabstop=4 expandtab",
-    }
-)
--- autocmd FileType css setlocal ts=2 sts=2 sw=2 expandtab 
-vim.api.nvim_create_autocmd(
-    { "FileType" },
-    {
-        pattern = "css",
-        command = "setlocal sw=2 softtabstop=2 expandtab",
-    }
-)
--- autocmd FileType python setlocal ts=4 sts=4 sw=4 expandtab 
-vim.api.nvim_create_autocmd(
-    { "FileType" },
-    {
-        pattern = "python",
-        command = "setlocal sw=4 softtabstop=4 expandtab",
-    }
+  { "FileType" },
+  {
+    pattern = "typescriptreact",
+    command = "setlocal sw=4 softtabstop=4 expandtab",
+  }
 )
 
 -- autocmd FileType css setlocal ts=2 sts=2 sw=2 expandtab 
 vim.api.nvim_create_autocmd(
-    { "FileType" },
-    {
-        pattern = "lua",
-        command = "setlocal sw=2 softtabstop=2 expandtab",
-    }
+  { "FileType" },
+  {
+    pattern = "css",
+    command = "setlocal sw=2 softtabstop=2 expandtab",
+  }
+)
+
+-- autocmd FileType python setlocal ts=4 sts=4 sw=4 expandtab 
+vim.api.nvim_create_autocmd(
+  { "FileType" },
+  {
+    pattern = "python",
+    command = "setlocal sw=4 softtabstop=4 expandtab",
+  }
+)
+
+-- autocmd FileType css setlocal ts=2 sts=2 sw=2 expandtab 
+vim.api.nvim_create_autocmd(
+  { "FileType" },
+  {
+    pattern = "lua",
+    command = "setlocal sw=2 softtabstop=2 expandtab",
+  }
 )
