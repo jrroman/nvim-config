5061c787603208a70cdace81018c1a73e1641846
John Roman <jrrmod@gmail.com>
2025-08-02 00:21:52 -0400

    add comment

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -8,6 +8,7 @@ vim.cmd("cabbrev E Explore")
 keymap("n", "<Leader>ff", ":Telescope find_files<CR>", default_opts)
 keymap("n", "<Leader>fg", ":Telescope live_grep<CR>", default_opts)
 keymap("n", "<Leader>fb", ":Telescope buffers<CR>", default_opts)
+-- nvim tree
 keymap("n", "<Leader>e", ":NvimTreeToggle<CR>", default_opts)
 -- fterm
 -- keymap('n', '<Leader>fo', '<CMD>lua require("FTerm").toggle()<CR>', default_opts)
