567cfa3dae14613370032e7a1dc4f13a0f10520a
John Roman <jrrmod@gmail.com>
2025-08-08 10:45:13 -0400

    add some comments for mappings, update / add a few harpoon mappings

--- lua/segfvult/mappings.lua
+++ lua/segfvult/mappings.lua
@@ -19,13 +19,18 @@ vim.keymap.set("i", "jj", "<ESC>", default_opts)
 -- Replace word under cursor.
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
+-- Move highlighted lines under cursor.
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv") 
+-- Keep cursor in same position when moving lines.
 vim.keymap.set("n", "J", "mzJ`z")
+-- Keep cursor in middle when paging up and down.
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
+-- Keep cursor in middle when searching for next match.
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
+-- Dont lose paste buffer, delete highlighted word to void register. 
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
 -- Nvim tree
