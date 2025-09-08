e11c8b27b6d7ffa79641e84176fc2f2628188094
jr <jrrmod@gmail.com>
2025-09-07 21:08:20 -0400

    fix bufferline weirdness with colors and transparency

--- lua/segfvult/plugins/bufferline.lua
+++ lua/segfvult/plugins/bufferline.lua
@@ -3,24 +3,12 @@ return {
   release = "v4.*",
   requires = 'nvim-tree/nvim-web-devicons',
   config = function()
-    require('bufferline').setup({
-      highlights = {
-        fill = {
-          fg = "NONE",
-          bg = "NONE",
-        },
-        background = {
-          fg = "NONE",
-          bg = "NONE",
-        },
-        tab = {
-          fg = "NONE",
-          bg = "NONE",
-        },
-      },
+    local bufferline = require('bufferline');
+    bufferline.setup({
       options = {
-        style_preset = 'minimal',
+        style_preset = bufferline.style_preset.minimal,
         numbers = "buffer_id",
+        themable = false,
       }
     })
   end
