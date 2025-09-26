044277469cf05bd8fd627724a3f40153c0672c40
jr <jrrmod@gmail.com>
2025-09-26 12:48:40 -0400

    add function to open nvim-tree in float, not currently using it but its nice to have when i want it.

--- lua/segfvult/plugins/nvim-tree.lua
+++ lua/segfvult/plugins/nvim-tree.lua
@@ -9,9 +9,27 @@ return {
     { "<leader>ee", ":NvimTreeFocus<CR>",    mode = "n" },
   },
   config = function()
+    local function open_win_config()
+      local win_w = vim.opt.columns:get()
+      local win_h = vim.opt.lines:get()
+      local tree_w = 80
+      local tree_h = math.floor(tree_w * win_h / win_w)
+
+      return {
+        border = "rounded",
+        relative = "editor",
+        width = tree_w,
+        height = tree_h,
+        col = math.floor((win_w - tree_w) / 2 - 1),
+        row = math.floor((win_h - tree_h) / 2 - 1),
+      }
+    end
+
     require("nvim-tree").setup({
       hijack_cursor = true,
       sync_root_with_cwd = true,
+      prefer_startup_root = true,
+
       sort = {
         sorter = "case_sensitive",
       },
@@ -19,6 +37,10 @@ return {
         width = 30,
         adaptive_size = true,
         preserve_window_proportions = true,
+        -- float = {
+        --   enable = true,
+        --   open_win_config = open_win_config,
+        -- },
       },
       update_focused_file = {
         enable = true,
