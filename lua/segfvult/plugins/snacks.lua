910c3eeabf0d34031921370c723d5aa4ae647251
jr <jrrmod@gmail.com>
2025-10-01 14:42:30 -0400

    give folke snacks a try with picker, explorer, scratch, and bigfile

---
+++ lua/segfvult/plugins/snacks.lua
@@ -0,0 +1,28 @@
+return {
+  "folke/snacks.nvim",
+  lazy = false,
+  priority = 1000,
+  opts = {
+    bigfile = {
+      enabled = true,
+      notify = true,
+      size = 1.5 * 1024 * 1024, -- 1.5MB
+    },
+    explorer = { enabled = true, replace_netrw = true },
+    picker = { enabled = true },
+    scratch = { enabled = true },
+  },
+  keys = {
+    { "<leader>e",   function() Snacks.explorer() end,         desc = "Toggle Explorer" },
+    { "<leader>ef",  function() Snacks.explorer.reveal() end,  desc = "Reveal Explorer" },
+    { "<leader>ff",  function() Snacks.picker.files() end,     desc = "Find Files" },
+    { "<leader>fg",  function() Snacks.picker.grep() end,      desc = "Find Grep" },
+    { "<leader>fb",  function() Snacks.picker.buffers() end,   desc = "Find Buffers" },
+    { "<leader>fp",  function() Snacks.picker.git_files() end, desc = "Find Git Files" },
+    { "<leader>fbs", function() Snacks.picker.lines() end,     desc = "Find Buffers Fuzzy" },
+    { "<leader>fgs", function() Snacks.picker.grep_word() end, desc = "Find Grep String" },
+    { "<leader>fh",  function() Snacks.picker.help() end,      desc = "Find Help Tags" },
+    { "<leader>.",   function() Snacks.scratch() end,          desc = "Toggle Scratch" },
+    { "<leader>ss",  function() Snacks.scratch.select() end,   desc = "Select Scratch" },
+  }
+}
--- lua/segfvult/plugins/telescope.lua
+++ lua/segfvult/plugins/telescope.lua
@@ -1,19 +1,19 @@
 return {
-  "nvim-telescope/telescope.nvim",
-  tag = "0.1.5",
-  dependencies = {
-    "nvim-lua/plenary.nvim"
-  },
-  keys = {
-    { "<leader>ff", ":Telescope find_files<CR>" },
-    { "<leader>fg", ":Telescope live_grep<CR>" },
-    { "<leader>fb", ":Telescope buffers<CR>" },
-    { "<leader>fp", ":Telescope git_files<CR>" },
-    { "<leader>fbs", ":Telescope current_buffer_fuzzy_find<CR>" },
-    { "<leader>fgs", ":Telescope grep_string<CR>" },
-    { "<leader>fh", ":Telescope help_tags<CR>" },
-  },
-  config = function()
-    require('telescope').setup({})
-  end
+  -- "nvim-telescope/telescope.nvim",
+  -- tag = "0.1.5",
+  -- dependencies = {
+  --   "nvim-lua/plenary.nvim"
+  -- },
+  -- keys = {
+  --   { "<leader>ff", ":Telescope find_files<CR>" },
+  --   { "<leader>fg", ":Telescope live_grep<CR>" },
+  --   { "<leader>fb", ":Telescope buffers<CR>" },
+  --   { "<leader>fp", ":Telescope git_files<CR>" },
+  --   { "<leader>fbs", ":Telescope current_buffer_fuzzy_find<CR>" },
+  --   { "<leader>fgs", ":Telescope grep_string<CR>" },
+  --   { "<leader>fh", ":Telescope help_tags<CR>" },
+  -- },
+  -- config = function()
+  --   require('telescope').setup({})
+  -- end
 }
