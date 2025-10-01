43518e8d0bdb108a474c607c82758465626c8ea9
jr <jrrmod@gmail.com>
2025-10-01 16:17:15 -0400

    stylua formatted snacks.lua

--- lua/segfvult/plugins/snacks.lua
+++ lua/segfvult/plugins/snacks.lua
@@ -13,16 +13,82 @@ return {
     scratch = { enabled = true },
   },
   keys = {
-    { "<leader>e",   function() Snacks.explorer() end,         desc = "Toggle Explorer" },
-    { "<leader>ef",  function() Snacks.explorer.reveal() end,  desc = "Reveal Explorer" },
-    { "<leader>ff",  function() Snacks.picker.files() end,     desc = "Find Files" },
-    { "<leader>fg",  function() Snacks.picker.grep() end,      desc = "Find Grep" },
-    { "<leader>fb",  function() Snacks.picker.buffers() end,   desc = "Find Buffers" },
-    { "<leader>fp",  function() Snacks.picker.git_files() end, desc = "Find Git Files" },
-    { "<leader>fbs", function() Snacks.picker.lines() end,     desc = "Find Buffers Fuzzy" },
-    { "<leader>fgs", function() Snacks.picker.grep_word() end, desc = "Find Grep String" },
-    { "<leader>fh",  function() Snacks.picker.help() end,      desc = "Find Help Tags" },
-    { "<leader>.",   function() Snacks.scratch() end,          desc = "Toggle Scratch" },
-    { "<leader>ss",  function() Snacks.scratch.select() end,   desc = "Select Scratch" },
-  }
+    {
+      "<leader>e",
+      function()
+        Snacks.explorer()
+      end,
+      desc = "Toggle Explorer",
+    },
+    {
+      "<leader>ef",
+      function()
+        Snacks.explorer.reveal()
+      end,
+      desc = "Reveal Explorer",
+    },
+    {
+      "<leader>ff",
+      function()
+        Snacks.picker.files()
+      end,
+      desc = "Find Files",
+    },
+    {
+      "<leader>fg",
+      function()
+        Snacks.picker.grep()
+      end,
+      desc = "Find Grep",
+    },
+    {
+      "<leader>fb",
+      function()
+        Snacks.picker.buffers()
+      end,
+      desc = "Find Buffers",
+    },
+    {
+      "<leader>fp",
+      function()
+        Snacks.picker.git_files()
+      end,
+      desc = "Find Git Files",
+    },
+    {
+      "<leader>fbs",
+      function()
+        Snacks.picker.lines()
+      end,
+      desc = "Find Buffers Fuzzy",
+    },
+    {
+      "<leader>fgs",
+      function()
+        Snacks.picker.grep_word()
+      end,
+      desc = "Find word under cursor",
+    },
+    {
+      "<leader>fh",
+      function()
+        Snacks.picker.help()
+      end,
+      desc = "Find Help Tags",
+    },
+    {
+      "<leader>.",
+      function()
+        Snacks.scratch()
+      end,
+      desc = "Toggle Scratch",
+    },
+    {
+      "<leader>ss",
+      function()
+        Snacks.scratch.select()
+      end,
+      desc = "Select Scratch",
+    },
+  },
 }
